25039541353e02a83d10a85817b76d14b89c1630
Bar Ben Haim <barbenbh@gmail.com>
2025-10-05 14:03:56 +0300

    fix deploy

--- src/middleware.js
+++ src/middleware.js
@@ -1,1 +1,16 @@
-export const config = { matcher: [] }
+import { NextResponse } from 'next/server'
+
+export function middleware(req) {
+    const url = req.nextUrl
+
+    if (url.pathname.startsWith('/api/entries')) {
+        url.pathname = url.pathname.replace('/api', '/_api')
+        return NextResponse.rewrite(url)
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ['/api/entries/:path*'],
+}
