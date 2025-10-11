1d463253668ec7d69f8af573258e4cd5d14dc64a
Bar Ben Haim <barbenbh@gmail.com>
2025-10-11 18:02:07 +0300

    fix credentials

--- src/lib/firebaseAdmin.js
+++ src/lib/firebaseAdmin.js
@@ -2,10 +2,15 @@ import { initializeApp, cert, getApps } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 import { getFirestore } from 'firebase-admin/firestore'
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+
 const app =
     getApps().length === 0
         ? initializeApp({
-              credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)),
+              credential: cert({
+                  ...serviceAccount,
+                  private_key: serviceAccount.private_key.replace(/\\n/g, '\n'),
+              }),
           })
         : getApps()[0]
 
